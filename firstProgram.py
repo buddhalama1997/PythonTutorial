032b35646fbaf1feccd7d41ddedd8e3c02663ad2
Buddha Lama <lamalakpa050@gmail.com>
2021-12-30 08:42:21 +0545

    Initial COmmit

---
+++ firstProgram.py
@@ -0,0 +1,17 @@
+# print hello world
+print('Hello World!')
+
+# Asking their name
+print('What is your name?')
+
+# allow to enter a name using keyboard
+myName = input()
+
+# print the greeting
+print('It is good to meet you '+ myName)
+
+# print length of name
+print('The length of your name is:')
+
+# print length
+print(len(myName))
